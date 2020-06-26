fa5d5bef7630e2a6004529eba4c5e88080cfad0a
Now_Sprouting <15542456716@163.com>
2020-06-26 16:36:25 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,16 @@
 Page({
-  
+  data: {
+    name: '丁同学',
+    star: [
+      { num: 30, name: 'Curry' },
+      { num: 11, name: 'Thompson' },
+      { num: 30, name: 'Durante' }
+    ],
+    count: 0
+  },
+  handleAddClick() {
+    this.setData({
+      count: this.data.count + 1
+    })
+  }
 })
