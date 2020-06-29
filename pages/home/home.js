1552c9972d6548a582b373d7e4830cec8a251f71
Now_Sprouting <15542456716@163.com>
2020-06-29 15:12:49 +0800

    弹窗和分享

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,17 @@
 Page({
+  handleShowToast(){
+   wx.showToast({
+     title: '成功',
+     icon:'',
+     image:"../../icon/fenlei.png"
+   })
+   },
+  onShareAppMessage(options){
+    return{
+      title:'hello',
+      path:'/pages/home/home.wxml',
+      imageUrl:'../../icon/foot1-copy.png'
+    }
+  }
   
 })
